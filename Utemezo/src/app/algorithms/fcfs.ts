f2ac082b50683d4e053ab3137b22fc299de20514
Mátyás <matyi0918@gmail.com>
2022-03-29 21:37:09 +0200

    Algorithms refactored

--- Utemezo/src/app/algorithms/fcfs.ts
+++ Utemezo/src/app/algorithms/fcfs.ts
@@ -3,29 +3,17 @@ import {SchedulerArgorithm} from "./scheduler-argorithm";
 export class Fcfs extends SchedulerArgorithm {
 
     calculate(): void {
-        let processesDone = 0;
-        let waitingProcesses: { pid: number, timeLeft: number }[] = [];
+        this.initializeAlgorithm();
 
-        this.result = [];
-        while (processesDone < this.processes.length) {
-            console.log(waitingProcesses);
-            let processesArriving = this.processes.filter(p => p.arrival == this.result.length);
-            waitingProcesses.push(...processesArriving.map(p => {
-                return {pid: this.processes.indexOf(p), timeLeft: p.cpu_time}
-            }));
+        while (this.processesDone < this.processes.length) {
+            this.addArrivalProcesses();
 
-            if (waitingProcesses.length > 0 && waitingProcesses[0].timeLeft == 0) {
-                waitingProcesses.shift();
-                processesDone++;
+            if (this.isFirstWorkingTaskPresentAndDone()) {
+                this.removeFirstWorkingTask();
             }
 
-            if (waitingProcesses.length > 0) {
-                waitingProcesses[0].timeLeft--;
-            }
-            this.result.push({
-                running: waitingProcesses[0]?.pid ?? null,
-                waiting: waitingProcesses.length > 1 ? waitingProcesses.slice(1, waitingProcesses.length).map(p => p.pid) : []
-            })
+            this.workOnFirstWorkingProcess();
+            this.recordTimeSlice();
         }
     }
 }
